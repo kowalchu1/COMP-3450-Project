44215238a01ebf9dc78bbf7fb0d8cfa38334efdf
kowalchu1 <kowalchu1@hotmail.com>
2021-02-23 18:42:33 -0800

    Inserting Share email code

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,6 +13,8 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
+import java.io.File;
+
 public class MainPage extends AppCompatActivity {
 
     @Override
@@ -20,17 +24,32 @@ public class MainPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.Share);
-        fab.setOnClickListener(new View.OnClickListener() {
+        Button timeButton = findViewById(R.id.TimeButton);
+        Button drillButton = findViewById(R.id.DrillButton);
+        Button eventButton = findViewById(R.id.EventButton);
+        FloatingActionButton share = findViewById(R.id.Share);
+
+
+        share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                //This is for sharing the file/data to the athlete
+                Intent intentShareFile = new Intent(Intent.ACTION_SEND);
+                File fileWithinMyDir = new File(/*Empty till database is created*/);
 
-                Button timeButton = findViewById(R.id.TimeButton);
-                Button drillButton = findViewById(R.id.DrillButton);
-                Button eventButton = findViewById(R.id.EventButton);
-                FloatingActionButton share = findViewById(R.id.Share);
+                if(fileWithinMyDir.exists()) {
+                    intentShareFile.setType("application/pdf");
+                    intentShareFile.putExtra(Intent.EXTRA_STREAM, Uri.parse("file:// //File goes here"));
+
+                    intentShareFile.putExtra(Intent.EXTRA_SUBJECT,
+                            "Sharing File...");
+                    intentShareFile.putExtra(Intent.EXTRA_TEXT, "Sharing File...");
+
+                    startActivity(Intent.createChooser(intentShareFile, "Share File"));
+                }
+
             }
         });
     }
