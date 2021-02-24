f6454416ac0ba8464ac88a9acbf84796bf35e562
kowalchu1 <kowalchu1@hotmail.com>
2021-02-23 18:34:03 -0800

    Button Implementation

--- app/src/main/java/com/example/myapplication/MainPage.java
+++ app/src/main/java/com/example/myapplication/MainPage.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
 
@@ -19,12 +20,17 @@ public class MainPage extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.Share);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
+                Button timeButton = findViewById(R.id.TimeButton);
+                Button drillButton = findViewById(R.id.DrillButton);
+                Button eventButton = findViewById(R.id.EventButton);
+                FloatingActionButton share = findViewById(R.id.Share);
             }
         });
     }
